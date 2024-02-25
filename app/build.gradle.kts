625de47be26ad9d7bc05ce16292fc2e3d06cd0de
mindyhsu <mindy840204@gmail.com>
2024-02-25 20:35:41 +0800

    Update targetSdk and compileSdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.mindyhsu.githubactionsdemo"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.mindyhsu.githubactionsdemo"
         minSdk = 28
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
